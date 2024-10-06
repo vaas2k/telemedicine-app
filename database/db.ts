ad31cc3467509aca36214f3c3d0d4bdfb5691101
Salman Ahmed <53785018+vaas2k@users.noreply.github.com>
2024-10-06 18:43:58 +0500

    Update db.ts

--- database/db.ts
+++ database/db.ts
@@ -3,7 +3,7 @@ import mongoose, { Mongoose } from 'mongoose';
 // In your connectToDatabase file:
 const connectToDatabase = async () => {
   console.log('Connecting...')
-  await mongoose.connect('mongodb+srv://salsuqe1:6AAEcIIA0mW6MyAK@hospital.hlc2kkw.mongodb.net/hospital?retryWrites=true&w=majority&appName=Cluster0')
+  await mongoose.connect('mongodbstringrequired')
   .then((data : any)=> {console.log('Connected')})
   .catch((error : any) => console.log("Not Connected"));
 };
